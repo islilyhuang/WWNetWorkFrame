c96021881b8b907b76b4ea9b29d76e4fa0c107db
黄力 <huangli@admaster.com.cn>
2015-12-14 11:07:19 +0800

    Release 1.0.0.

---
+++ WWNetWorkFrameDemo/WWNetWorkFrameDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWNetWorkFrameDemo
+//
+//  Created by 黄力 on 15/12/14.
+//  Copyright © 2015年 黄力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWNetWorkFrameDemo/WWNetWorkFrameDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWNetWorkFrameDemo
+//
+//  Created by 黄力 on 15/12/14.
+//  Copyright © 2015年 黄力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
